cc82b27754a374b7f4e4ebbfd843566e33d1d692
Boerilorenzo03 <117928573+Boerilorenzo03@users.noreply.github.com>
2022-11-15 16:37:33 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+
 using namespace std;
 
 int main() {
-   return 0;
+    int a, b, x, y;
+    cout << "inserire valori: a, b, x, y" << endl;
+    cin >>a>>b>>x>>y;
+    if (x<0 && y<0) {
+        cout << "z = " << (a*x)-(b*y)<< endl;
+
+    }
+    else if (x>=0 && y<=0) {
+        cout << "z = " << (a*x*x)-(b*y) << endl;
+    }
+    else cout << "z = " << (a*x)+(b*y*y) << endl;
+    return 0;
 }
